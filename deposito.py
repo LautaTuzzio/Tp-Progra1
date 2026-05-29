262777a0b2ea522ef3f272b0d6b9ab951862970d
SantinoMilei <santinomilei07@gmail.com>
2026-05-29 18:25:11 -0300

    Agrego funcion deposito

--- deposito.py
+++
@@ -1,8 +0,0 @@
-def depositar_saldo(saldo_actual, monto_deposito):
-    float(input(monto_deposito))
-    if monto_deposito <= 0:
-        print("El monto a depositar debe ser mayor a 0") 
-
-    nuevo_saldo = saldo_actual + monto_deposito
-    return nuevo_saldo
-depositar_saldo
